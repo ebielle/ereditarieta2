d6c1fd676253217f305f6b9a8969404e1237c5a3
ebielle <eryblongo@gmail.com>
2024-04-08 21:38:37 +0200

    finished exercise

---
+++ src/Forma.java
@@ -0,0 +1,8 @@
+public abstract class Forma {
+
+    public static int calcolaArea() {
+        return 0;
+    }
+
+    public abstract int calcolaArea(int x, int y);
+}
---
+++ src/Rettangolo.java
@@ -0,0 +1,7 @@
+public class Rettangolo extends Forma {
+
+    @Override
+    public int calcolaArea(int x, int y) {
+        return x + y;
+    }
+}
