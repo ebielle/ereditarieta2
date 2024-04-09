745c7d578de900db1c06e8a93d2fa5dfeede1514
ebielle <eryblongo@gmail.com>
2024-04-09 10:24:39 +0200

    fixed fr

--- src/Forma.java
+++ src/Forma.java
@@ -1,8 +1,6 @@
-public abstract class Forma {
+public class Forma {
 
-    public static int calcolaArea() {
+    public int calcolaArea(int x, int y) {
         return 0;
     }
-
-    public abstract int calcolaArea(int x, int y);
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Forma forma = new Forma() {
-            @Override
-            public int calcolaArea(int x, int y) {
-                return 0;
-            }
-        };
+        Forma forma = new Forma();
         Rettangolo rettangolo = new Rettangolo();
 
-        System.out.println("L'area è: " + forma.calcolaArea());
+        System.out.println("L'area è: " + forma.calcolaArea(0, 0));
         System.out.println("L'area è: " + rettangolo.calcolaArea(2, 3));
 
     }
